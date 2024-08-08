c51350c407f585de05e569b0832be6dd8a67c8f1
bayekOp <bayeksiwa2447@gmail.com>
2024-08-08 11:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function selectGender(gender) {
+    if (gender === 'female') {
+        window.location.href = 'female.html';
+    } else if (gender === 'male') {
+        window.location.href = 'male.html';
+    }
+}
